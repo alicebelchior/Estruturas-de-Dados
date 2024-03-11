ebef8551de6609ae8ddc9297c0282f95d8b428b6
alicebelchior <alice.belchior23@gmail.com>
2024-03-11 11:12:08 -0300

    Exercícios de dicionários

--- Dicionários/exercícios dicionario/ex1.py
+++ Dicionários/exercícios dicionario/ex1.py
@@ -8,9 +8,39 @@
 # criando um dicionario
 dici = {}
 
-
 # add chaves e valores
-dici["titulo"] = ["Eu sou o mensageiro", ""]
-dici["autor"] = []
-dici["ano"] = []
-dici["páginas"] = []
+dici["titulo"] = ["Eu sou o mensageiro", "Crepúculo", "A revolução dos bichos", "Azul é a cor mais quente", "Persépolis"]
+dici["autor"] = ["Markus Zusak", "Stephenie Meyer", "George Orwell", "Julie Maroh", "Marjane Satrapi"]
+dici["ano"] = [2002, 2005, 1945, 2010, 2000]
+dici["páginas"] = [320, 480, 152, 160, 352]
+
+# listando os dados armazenados
+for chave in dici:
+     print(f'\n {chave} - {dici[chave]}')
+
+print('\n\n')
+    
+# adicionando dois livros
+# livro 1
+dici["titulo"].append("Casos de Família")
+dici["autor"].append("Ilana Casoy")
+dici["ano"].append(2016)
+dici["páginas"].append(528)
+
+# livro 2
+dici["titulo"].append("Meu amigo Dahmer")
+dici["autor"].append("Derf Beckderf")
+dici["ano"].append(2012)
+dici["páginas"].append(288)
+
+# modificando o autor do 2º livro
+dici["autor"][1] = "Anne Rice"
+for chave in dici:
+     print(f'\n {chave} - {dici[chave]}')
+        
+# imprimindo chaves e valores
+for chave in dici:
+     print(f'\n{chave}')
+     for valor in dici[chave]:
+          print(f'  - {valor}')
+          
