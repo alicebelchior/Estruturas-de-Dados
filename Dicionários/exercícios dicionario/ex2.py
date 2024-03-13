f9354f3199b165371e34772635f87b3ad3aab0f9
alicebelchior <alice.belchior23@gmail.com>
2024-03-13 10:25:16 -0300

    Exercício de dicionário

--- Dicionários/exercícios dicionario/ex2.py
+++ Dicionários/exercícios dicionario/ex2.py
@@ -3,3 +3,23 @@
 # criando um dicionario
 dici = {}
 
+#chave (disciplina) e valores (notas de 4 provas)
+dici["português"] = [60, 75, 83, 90]
+dici["matemática"] = [45, 88, 79, 62]
+dici["história"] = [93, 79, 35, 45]
+dici["geografia"] = [85, 17, 51, 59]
+dici["biologia"] = [55, 84, 66, 74]
+dici["química"] = [99, 85, 74, 17]
+
+#variaveis para obter a média
+soma = 0
+media = 0
+
+for disciplina in dici:
+     for nota in dici[disciplina]:
+          soma += nota
+
+#imprimindo valor da media     
+for disciplina in dici:
+     media = soma / 4
+     print(f'\n {disciplina} - {media}')
