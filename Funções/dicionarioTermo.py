0ed7573c725723f0be1bd55d0d2977de27230b71
alicebelchior <alice.belchior23@gmail.com>
2024-04-15 11:10:54 -0300

    Exercícios de função

---
+++ Funções/dicionarioTermo.py
@@ -0,0 +1,6 @@
+# Crie um script em Python onde deverá haver um dicionário de termos, no programa o dicionário deverá receber "n" termos , cada termo poderá receber de uma ou mais definições. Crie o programa seguindo as seguintes especificações:
+     # Novos termos devem ser inseridos no dicionário durante a execução do programa;
+     # Novas definições dos termos devem ser adicionadas durante a execução do programa, um termo deve ter no mínimo um termo e no máximo "n" termos;
+     # Um termo pode ser acessado e deve exibir todas as suas definições; 
+     # O programa deve permitir a exclusão de um ou mais termos quando o usuário indicar o termo;
+     
---
+++ Funções/exercícios Funções/ex5.py
@@ -0,0 +1,2 @@
+# Escreva um script em Python para simular o funcionamento de uma lâmpada, nesse programa o usuário poderá ligar e desligar a lâmpada, o programa deverá informar se a lâmpada está ligada ou desligada. Monte este programa utilizando funções.
+
