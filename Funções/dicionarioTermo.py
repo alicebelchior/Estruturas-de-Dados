b22baced0dc45e88527656f48ceed91bbb558c3f
GHasner <guilherme.h.ceet.ti@gmail.com>
2024-05-28 11:14:06 -0300

    Dicionário de termos

--- Funções/dicionarioTermo.py
+++ Funções/dicionarioTermo.py
@@ -3,12 +3,60 @@
      # Novas definições dos termos devem ser adicionadas durante a execução do programa, um termo deve ter no mínimo um termo e no máximo "n" termos;
      # Um termo pode ser acessado e deve exibir todas as suas definições; 
      # O programa deve permitir a exclusão de um ou mais termos quando o usuário indicar o termo;
-     
-dicionario = []
 
-opcao = int(input("Escolha uma opção: \n"
-                       "1 - Adicionar palavra\n"
-                       "2 - Ler palavra\n"
-                       "3 - Excluir palavra\n"
+# dicionario vazio    
+dicionario = {}
+
+# loop para manter o programa rodando
+while True:
+     opcao = int(input("Escolha uma opção do menu: \n"
+                       "1 - Adicionar termo e definição\n"
+                       "2 - Ler os termos\n"
+                       "3 - Excluir termo\n"
                        "4 - Sair\n"))
-
+     
+     # add novo termo
+     if opcao == 1:
+          # cada termo vai ter "n" definições
+          definicoes = []
+          
+          # entrada do novo termo
+          termo = input("Digite um termo: \n")
+                    
+          # loop para manter o trecho de adição de definição rodando
+          while True:
+               definicao = input("Digite uma definição para o termo acima: \n")
+               definicoes.append(definicao)
+               
+               continuar = input("Deseja continuar (s/n)? ")
+               print()
+               if continuar.lower() == "n":
+                    break
+          
+          # adicionando o termo e suas definições ao dicionário (chave: valor)
+          dicionario[termo.lower()] = definicoes #.lower() = deixa tudo em minusculo
+          print("Dicionário atualizado!")
+     
+     # ler termo
+     elif opcao == 2:
+          # mostrando os termos do dicionario
+          for termo in dicionario:
+               print(termo)
+          
+          # mostrando as definições dos termos
+          leitor = input("Digite o termo cujas definições você deseja ler: \n")
+          if leitor == termo:
+               
+          
+          
+     # excluir termo
+     # elif opcao == 3:
+          
+          
+     # sair
+     elif opcao == 4:
+          print("Poxa, já? Então, tá. Tchau!")
+          break
+     
+     else:
+          print("Essa opção não existe, digite uma opção válida.")
