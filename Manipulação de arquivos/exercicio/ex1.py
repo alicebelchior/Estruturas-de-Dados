e0f12e96a43549370e13e985a0433bb5370f2efc
GHasner <guilherme.h.ceet.ti@gmail.com>
2024-05-29 11:12:31 -0300

    Exercícios de manipulação de arquivo

---
+++ Manipulação de arquivos/exercicio/ex1.py
@@ -0,0 +1,14 @@
+# Crie um programa em Python que deverá utilizar funções e gravar seus dados em arquivo, o programa deverá ainda apresentar as seguintes características e funcionalidades:
+# ● Exibir um menu com 3 opções:
+     # ○ inserir palavra,
+     # ○ Listar palavras
+     # ○ Sair do programa.
+# ● Ser capaz de receber uma palavra por vez;
+# ● Armazenar “n” palavras em uma lista;
+# ● Exibir todas as palavras armazenadas, uma palavra por linha;
+# ● O programa deve gravar os dados contidos na lista no momento que for finalizado;
+# ● Ao inicializar o programa deve carregar os dados gravados na última execução;
+# ● As operações devem ser feitas com a utilização de funções
+     # ○ Gravação
+     # ○ Leitura
+     # ○ Menu(opcional)
