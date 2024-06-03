959fa8d2528a5be01d8dde5c798f34d75488cc10
alicebelchior <alice.belchior23@gmail.com>
2024-06-02 23:18:41 -0300

    exercicio 1 de manipulação de arquivo - gravador de palavra

--- Manipulação de arquivos/exercicio/ex1.py
+++ Manipulação de arquivos/exercicio/ex1.py
@@ -12,3 +12,50 @@
      # ○ Gravação
      # ○ Leitura
      # ○ Menu(opcional)
+
+# lista de palavras vazias
+lista_palavra = []
+
+def gravacao():
+     with open("palavra.txt", "w") as arquivo:
+          # loop para escrever as palavras no arquivo
+          for palavra in lista_palavra:
+               arquivo.write(f"{palavra} \n")
+     
+def leitura():
+     with open ("palavra.txt", "r") as arquivo:
+          conteudo = arquivo.read()
+          print(conteudo)          
+     
+def menu():
+     # essa função sempre vai mostrar as opções inserir, listar ou sair
+     print("1. Inserir palavra \n"
+           "2. Listar palavra \n"
+           "3. Sair")
+     
+
+# programa principal
+while True:
+     # chamando a função menu
+     menu()
+     opcao = int(input("Escolha uma opção: "))
+     
+     # add palavra
+     if opcao == 1:
+          print("-" *20)
+          palavra = input("Digite uma palavra: \n")
+          lista_palavra.append(palavra)
+          print("Palavra adicionada! \n")
+     
+     # listar palavra   
+     elif opcao == 2:
+          
+          
+     # sair  
+     elif opcao == 3:
+          
+          # gravar os dados contidos na lista no momento que for finalizado
+          gravacao()
+          
+     else:
+          print("Essa opção não existe, digite uma opção válida.")
